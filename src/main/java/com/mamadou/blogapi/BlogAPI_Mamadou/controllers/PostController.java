8de8a21e39cfd7711ec005e050b9f1801c8b331d
mamadou8bah <mbah18791@gmail.com>
2025-07-09 18:30:30 +0000

    Implemented the enpoints for a blog post

--- src/main/java/com/mamadou/blogapi/BlogAPI_Mamadou/controllers/PostController.java
+++ src/main/java/com/mamadou/blogapi/BlogAPI_Mamadou/controllers/PostController.java
@@ -1,11 +1,47 @@
 package com.mamadou.blogapi.BlogAPI_Mamadou.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mamadou.blogapi.BlogAPI_Mamadou.models.Post;
+import com.mamadou.blogapi.BlogAPI_Mamadou.models.User;
+import com.mamadou.blogapi.BlogAPI_Mamadou.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
 public class PostController {
+
+    @Autowired
+    PostService service;
+
+
+    @PostMapping
+    public Post createPost( @RequestBody Post post){
+        return service.addPost(post);
+    }
+
+    @GetMapping
+    public List<Post>getPosts(){
+        return service.getPosts();
+    }
+
+    @GetMapping("/user")
+    public List<Post>getPostByUser(@RequestBody User user){
+        return service.findPostByUser(user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable int id) {
+        boolean deleted = service.deletePost(id);
+        if (deleted) {
+            return ResponseEntity.ok("Post deleted successfully.");
+        } else {
+            return ResponseEntity.status(404).body("Post not found.");
+        }
+    }
 
 }
--- src/main/java/com/mamadou/blogapi/BlogAPI_Mamadou/services/PostService.java
+++ src/main/java/com/mamadou/blogapi/BlogAPI_Mamadou/services/PostService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -31,9 +32,14 @@ public class PostService {
         return repo.findById(id).orElseThrow(()->new RuntimeException("Could not find post"));
     }
 
-    public Post deletePost(int id){
-        Post post=repo.findById(id).orElseThrow(()->new RuntimeException("Could not find post"));
-        repo.delete(post);
-        return post;
+    public boolean deletePost(int id) {
+        Optional<Post> post = repo.findById(id);
+        if (post.isPresent()) {
+            repo.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
+
 }
